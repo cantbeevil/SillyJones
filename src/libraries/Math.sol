034db4609286418f43798c5aba6b890053ef6cc3
Test <test@Tests-MacBook-Pro.local>
2022-02-26 10:41:15 -0800

    library

---
+++ src/libraries/Math.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.2;
+
+library SimpleMath {
+    // From Solemate
+    function fmul(
+        uint256 x,
+        uint256 y,
+        uint256 baseUnit
+    ) internal pure returns (uint256 z) {
+        assembly {
+            // Store x * y in z for now.
+            z := mul(x, y)
+
+            // Equivalent to require(x == 0 || (x * y) / x == y)
+            if iszero(or(iszero(x), eq(div(z, x), y))) {
+                revert(0, 0)
+            }
+
+            // If baseUnit is zero this will return zero instead of reverting.
+            z := div(z, baseUnit)
+        }
+    }
+}
